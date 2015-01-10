abd122beed8af789bc291e931c9aac0837395b04
MANISH MALLICK <manishmallick@MANISHs-MacBook-Pro.local>
2015-01-10 14:38:14 -0600

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by MANISH MALLICK on 1/10/15.
+//  Copyright (c) 2015 MANISH MALLICK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by MANISH MALLICK on 1/10/15.
+//  Copyright (c) 2015 MANISH MALLICK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
